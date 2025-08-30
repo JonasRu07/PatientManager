af7cf63f63eb2445f5beb37f60967562d0a9bc61
Jonas Ruppert <rpprtjns@gmail.com>
2025-08-30 23:37:09 +0200

    Init Week

--- manager/__init__.py
+++ manager/__init__.py
@@ -1,3 +1,4 @@
 from .hour import Hour
 from .patient import Patient
 from .static_io import InputHours, InputPatients
+from .week import Week
---
+++ manager/week.py
@@ -0,0 +1,27 @@
+import os
+import copy
+
+from .static_io import InputHours
+from .hour import Hour
+
+class Week:
+    """
+    Representation of the working week. It loads all hours given in the config file, 
+    if not given hours directly
+    path: str: (optional) Path to the config file to load the hors from
+    hours: list[Hour,]: (optional) Hours containing the week
+    """
+    def __init__(self, path:str=os.path.join(".", "configs", "hours.json"), hours:list[Hour, ]=[]):
+        if hours == []:
+            self.hours = InputHours.load(path)
+        else:
+            self.hours = hours
+
+    def copy(self):
+        """
+        Copies the content of the week directly. Preventing any shenanigans with changing one hour 
+        """
+        return type(self)(hours=[copy.deepcopy(hour) for hour in self.hours])
+
+    def __repr__(self):
+        return f"Week:\n{[h.taken_by.name if h.taken_by is not None else None for h in self.hours]}"
