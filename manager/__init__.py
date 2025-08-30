cb41e8f8c47d46fbb1b6d753368186f3922d06c5
Jonas Ruppert <rpprtjns@gmail.com>
2025-08-30 22:49:35 +0200

    Init patient

--- manager/__init__.py
+++ manager/__init__.py
@@ -1,1 +1,2 @@
 from .hour import Hour
+from .patient import Patient
---
+++ manager/patient.py
@@ -0,0 +1,10 @@
+class Patient:
+    def __init__(self, name:str, pos_times:list[int, ]):
+        """
+        Representation of a patient.
+        name: str: Name of the patient. Should only be used with 'UTF-8' encoding
+        pos_times: list[int,]: The patient has a number of possible hours, s/he 
+                               can attend to. The integers are the IDs of the Hour.
+        """
+        self.name = name
+        self.pos_times = pos_times
