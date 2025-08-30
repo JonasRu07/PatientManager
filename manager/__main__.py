abcf5a2059b48c396ca1d2081a86a4e2aae96dd5
JonasRu07 <rpprtjns@gmail.com>
2025-08-30 22:11:51 +0200

    Setup

---
+++ manager/__main__.py
@@ -0,0 +1,8 @@
+import manager
+
+def main():
+    print('Start Patient Manager')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,13 @@
+# Build and tested in python 3.12.3
+
+from setuptools import setup, find_packages
+
+    
+VERSION = '0.0.1'
+
+
+setup(name='Patient Manager',
+      version=VERSION,
+      author='Jonas Ruppert',
+      author_email='rpprtjns@gmail.com',
+      url='https://github.com/JonasRu07/PatientManager')
