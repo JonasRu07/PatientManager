9aef6a2038909735c8c15da99c527a1f38fc283f
Jonas Ruppert <rpprtjns@gmail.com>
2025-08-31 00:15:15 +0200

    Working programm

--- manager/__main__.py
+++ manager/__main__.py
@@ -1,7 +1,22 @@
+import argparse
+
 import manager
 
-def main():
+def main(args):
     print("Start Patient Manager")
+    controller = manager.Controller()
+    if args.define:
+        controller.solve_define_answers()
+        print('Define solution\n', controller.week)
+    if args.complete:
+        print(controller.solve_recursive())
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--define", "-d", action="store_true", help="Solves all patients, which have an possible hour no other patient can attend to")
+    parser.add_argument("--complete", "-c", action='store_true', help="Tries to find a solution, which all patients are in one the week")
+    
+    args = parser.parse_args()
+    main(args)
