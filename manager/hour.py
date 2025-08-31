254419bea996eb2d528b4262e174aff9dfa0a62f
Jonas Ruppert <rpprtjns@gmail.com>
2025-08-31 12:19:46 +0200

    Typeanotations fix

--- manager/hour.py
+++ manager/hour.py
@@ -1,9 +1,11 @@
+from .patient import Patient
+
 
 class Hour:
     def __init__(self, ID:int, time:str, duration:int):
         """
-        Implemenatation of the time unit, which can be taken by a Patient.
-        ID: int: Unique indentification of the hour. Is made up of an 8-Bit integer.
+        Implementation of the time unit, which can be taken by a Patient.
+        ID: int: Unique identification of the hour. Is made up of an 8-Bit integer.
                  The first 4 bits define the day, the latter 4 the unique ID of the hour on this day
         time: str: When does the hour start. ex "0750"
         duration: int: How long the hour is in minutes 
@@ -11,4 +13,4 @@ class Hour:
         self.ID = ID
         self.time = time
         self.duration = duration
-        self.taken_by = None
+        self.taken_by:None | Patient = None
