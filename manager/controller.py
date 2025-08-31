80ee88ab231aabf24b65c66f09b895183854214e
Jonas Ruppert <rpprtjns@gmail.com>
2025-08-31 12:19:20 +0200

    Wrapper for recursive solving

--- manager/controller.py
+++ manager/controller.py
@@ -11,9 +11,9 @@ class Controller:
 
         self.week = Week(os.path.join(self.config_path, "hours.json"))
         print(f"Loaded {len(self.week.hours)} hours from config")
-        self.patient_manager = PatientWrapper(os.path.join(self.config_path, "patients.json"))
+        self.patient_wrapper = PatientWrapper(os.path.join(self.config_path, "patients.json"))
         self.patient_data = InputPatients.get_data(os.path.join(self.config_path, "patients.json"))
-        print(f"Loaded {len(self.patient_manager.patients)} patients from config")
+        print(f"Loaded {len(self.patient_wrapper.patients)} patients from config")
         
     def solve_define_answers(self) -> None:
         """
@@ -24,7 +24,7 @@ class Controller:
         for hour in self.week.hours:
             patient_index = None
             patient_ref = None
-            for index, patient in enumerate(self.patient_manager.patients):
+            for index, patient in enumerate(self.patient_wrapper.patients):
                 if hour.ID in patient.pos_times:
                     if patient_index is None:
                         patient_index = index
@@ -36,12 +36,15 @@ class Controller:
                 if patient_ref is not None:
                     print(f"Patient {patient_ref.name} has hour {hour.ID}")
                     hour.taken_by = patient_ref
-                    self.patient_manager.patients.remove(patient_ref)
+                    self.patient_wrapper.patients.remove(patient_ref)
                     changes = True
 
         return self.solve_define_answers() if changes else None
     
-    def solve_recursive(self, pm:PatientWrapper, week:Week, start=0) -> list[Week,]:
+    def solve_recursive(self) -> list[Week,]:
+        return self.__solve_recursive(self.patient_wrapper, self.week)
+    
+    def __solve_recursive(self, pw:PatientWrapper, week:Week, start=0) -> list[Week,]:
         """
         Recursively generates all possible solutions to the calender and return them
         :param pm: PatientWrapper
@@ -50,17 +53,17 @@ class Controller:
                                       generated before
         :return: list[Week,] Empty if no solution has been found. Each week is completely deep copied, incl. content
         """
-        if len(pm.patients) == 0:
+        if len(pw.patients) == 0:
             return [week]
         solutions = []
         for hour_index, hour in enumerate(week.hours):
             if hour_index < start or hour.taken_by is not None: continue
-            for patient_index, patient in enumerate(pm.patients):
+            for patient_index, patient in enumerate(pw.patients):
                 if hour.ID in patient.pos_times:
                     new_week = week.copy()
                     new_week.hours[hour_index].taken_by = patient
-                    new_pm = pm.copy()
-                    new_pm.patients.pop(patient_index)
-                    solutions += self.solve_recursive(new_pm, new_week, start=hour_index)
+                    new_pw = pw.copy()
+                    new_pw.patients.pop(patient_index)
+                    solutions += self.__solve_recursive(new_pw, new_week, start=hour_index)
 
         return solutions
