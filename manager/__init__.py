88e386b5288abeddd48a387b1e54fabce33851f6
JonasRu07 <rpprtjns@gmail.com>
2025-09-15 21:13:56 +0200

    Added missing import

--- manager/__init__.py
+++ manager/__init__.py
@@ -2,6 +2,7 @@ from .controller import Controller
 from .gui import MainGUI, AddPatientGUI, PatientManagingGUI
 from .hour import Hour
 from .patient import Patient
+from .patient_manager import PatientManager
 from .patient_wrapper import PatientWrapper
 from .static_io import InputHours, InputPatients
 from .ui_controller import UIController
