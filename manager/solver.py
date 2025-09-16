cb5b34daae42ec1d49d618c61d794f5113c09a45
JonasRu07 <rpprtjns@gmail.com>
2025-09-16 19:34:10 +0200

    Documentaion

--- manager/solver.py
+++ manager/solver.py
@@ -10,10 +10,47 @@ class Solver:
         self.week = week
         
     def define_answers(self, max_iterations:int=-1) -> Week:
-        return self.__define_answers(self.patient_manager.get_patients_inside_wrapper().copy().patients, self.week.copy(), max_iterations)
+        """
+        This function tries to find every hour, which can only be taken
+        up by one patient and assigns this patient to the hour. If more
+        than one patient has the hour as possible, no one will be 
+        assigned to to the hour.
+
+        Args:
+            max_iterations (int, optional): Optional limit on attempts 
+            to find a solution. Every negative number and 0 are 
+            interpreted as no limit, meaning the function will as long
+            as it can find new hours with only on patient. Defaults to -1.
+
+        Returns:
+            Week: the solution found
+        """
+        return self.__define_answers(
+            self.patient_manager.get_patients_inside_wrapper().copy().patients,
+            self.week.copy(),
+            max_iterations)
             
-        
-    def __define_answers(self, remaining_patients:list[Patient,], week:Week, max_iterations) -> Week:
+    def __define_answers(self, remaining_patients:list[Patient,],
+                         week:Week,
+                         max_iterations=-1) -> Week:
+        """
+        Internal function for handling the logic of the 
+        self.define_solution function
+
+        Args:
+            remaining_patients (list[Patient,]): a list with all 
+                patients to assign hours
+            week (Week): all available hours
+            max_iterations (int):
+            max_iterations (int, optional): Optional limit on attempts 
+                to find a solution. Every negative number and 0 are 
+                interpreted as no limit, meaning the function will as 
+                long as it can find new hours with only on patient. 
+                Defaults to -1. 
+
+        Returns:
+            Week: Found solution 
+        """
         if max_iterations == 0:
             return week
         
@@ -36,16 +73,24 @@ class Solver:
         return self.__define_answers(remaining_patients, week, max_iterations-1) if changes else week
     
     def all_solutions(self) -> list[Week]:
+        """
+        This function finds all solution. That's mean every patient
+        has an hour assigned to it.
+
+        Returns:
+            list[Week]: Every solution found. Empty list if no solution
+                found
+        """
         return self.__all_solutions(self.patient_manager.get_patients_inside_wrapper().copy(), self.week.copy())
     
     def __all_solutions(self, pw:PatientWrapper, week:Week, start=0) -> list[Week]:
         """
-        Recursively generates all possible solutions to the calender and return them
-        :param pw: PatientWrapper
-        :param week: Week
-        :param start : int [optional] Not needed for starting. Helps to cut out hours, which has been tested and
-                                      generated before
-        :return: list[Week,] Empty if no solution has been found. Each week is completely deep copied, incl. content
+        Internal function for handling the logic of the 
+        self.__all_solution function
+
+        Returns:
+            list[Week]: Every solution found. Empty list if no solution
+                found
         """
         if len(pw.patients) == 0:
             return [week]
