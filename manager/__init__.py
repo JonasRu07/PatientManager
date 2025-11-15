b99eaea719630e3d39ccfbadc131a335da00c7b0
JonasRu07 <rpprtjns@gmail.com>
2025-11-15 12:08:53 +0100

    Added missing Classes

--- manager/__init__.py
+++ manager/__init__.py
@@ -3,8 +3,8 @@ from .hour import Hour
 from .patient import Patient
 from .patient_manager import PatientManager
 from .patient_wrapper import PatientWrapper
-from .solver import Solver
-from .static_io import InputHours, InputPatients
+from .solver import Solver, SolutionPath
+from .static_io import InputHours, InputPatients, InputPlan, ConstEvoParams
 from .ui import MainUI
 from .ui_controller import UIController
 from .week import Week
